6df5cd8801c3582f90ac2136521eaaba1f38d0f9
private <user@example.com>
2015-11-05 14:59:34 +0000

    Add methods to update the board with user input.

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,14 +1,31 @@
 public class Game {
 
-    private Board board = new Board(2);
+    private Board board      = new Board(3);
+    private ConsoleUI gameUI = new ConsoleUI(new GameConsole(System.in, System.out));
 
     public Board getBoard() {
         return board;
     }
 
+    }
+
+    public int stringToNumber(String cellIndex) {
+        return Integer.parseInt(cellIndex);
+    }
+
+    public int getRowFromIndex(int index) {
+        return index / board.getSize();
+    }
+
+    public int getColFromIndex(int index) {
+        return index % board.getSize();
+    }
+
+    public String getMark(boolean first) {
+        return first ? "X" : "O";
+    }
+
     public void updateBoard(int i, int j, String mark) {
         board.setCell(i, j, mark);
     }
-
-
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,12 +1,41 @@
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class GameTest {
+
+    private Game game;
+
+    @Before
+    public void setUp() {
+        game = new Game();
+    }
+
+    @Test
+    public void convertsStringIntoNumber() {
+        assertEquals(1, game.stringToNumber("1"));
+    }
+
+    @Test
+    public void getsRowFromCellIndex() {
+        assertEquals(1 / game.getBoard().getSize(), game.getRowFromIndex(1));
+    }
+
+    @Test
+    public void getsColFromCellIndex() {
+        assertEquals(1 % game.getBoard().getSize(), game.getColFromIndex(1));
+    }
+
+    @Test
+    public void getsMark() {
+        assertEquals("X", game.getMark(true));
+        assertEquals("O", game.getMark(false));
+    }
+
     @Test
     public void updatesBoard() {
-        Game game = new Game();
         game.updateBoard(0, 0, "X");
         assertEquals("X", game.getBoard().getCell(0, 0));
     }
