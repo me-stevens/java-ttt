3353638158e977aeb87b05098c0a7f90ee59e55d
private <user@example.com>
2015-11-18 15:06:21 +0000

    Add changes to the menu to take into account the alien player

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -120,6 +120,13 @@ public class GameTest {
     }
 
     @Test
+    public void setsOneAlienPlayer() {
+        game.setPlayers("4");
+        assertTrue(game.getPlayer1() instanceof HumanPlayer);
+        assertTrue(game.getPlayer2() instanceof AlienPlayer);
+    }
+
+    @Test
     public void startPrintsWelcomeMessage() {
         spy.setInputs("1", "1", "4", "2", "5", "3");
         game.start();
@@ -158,7 +165,7 @@ public class GameTest {
     @Test
     public void alienPlays() {
         spy.setInput("1");
-        game.setPlayers("2");
+        game.setPlayers("4");
         playTurns(2);
     }
 
