d94766c82e0aee9f6f02339fea736a1cf299854a
me-stevens <me-stevens@users.noreply.github.com>
2015-11-18 17:52:38 +0000

    Creates a Menu class and moves the method to set the option from Game to Menu.

--- src/test/java/MenuTest.java
+++ src/test/java/MenuTest.java
@@ -1,2 +1,25 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class MenuTest {
+
+    private SpyConsole spy;
+    private Menu menu;
+
+    @Before
+    public void setUp() {
+        spy  = new SpyConsole();
+        menu = new Menu(new UserInterface(spy));
+    }
+
+    @Test
+    public void showPlayersMenuUntilRightOption() {
+        spy.setInputs("khgj", "0", "1");
+        assertEquals("1", menu.setOption());
+        assertEquals(3, spy.timesReadWasCalled());
+    }
+
 }
