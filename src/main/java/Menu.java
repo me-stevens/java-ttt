4b364aa59cfb48c3697e8eecc5fc99c9e2e202dc
me-stevens <me-stevens@users.noreply.github.com>
2015-11-25 14:35:43 +0000

    Get rid of the switch statement in the menu class:  - Get rid of the enum.  - Use a Hash Map instead.  - Add new methods to create the players according to option value.

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,41 +1,50 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class Menu {
 
     private UserInterface gameUI;
-
-    public enum Options {
-        TWO_HUMANS, HUMAN_AND_ROBOT, TWO_ROBOTS, HUMAN_AND_ALIEN
-    }
+    private HashMap<String, List<Player>> options;
 
     public Menu(UserInterface gameUI) {
         this.gameUI = gameUI;
+        options     = new HashMap<String, List<Player>>();
+        twoHumans();
+        humanAndRobot();
+        twoRobots();
+        humanAndAlien();
     }
 
     public List<Player> createPlayers() {
+        String option = setOption();
+
+        return options.get(option);
+    }
+
+    private void twoHumans() {
         Player player1 = new HumanPlayer(gameUI, "X");
         Player player2 = new HumanPlayer(gameUI, "O");
+        options.put("1", Arrays.asList(player1, player2));
+    }
 
-        char option = setOption().charAt(0);
+    private void humanAndRobot() {
+        Player player1 = new HumanPlayer(gameUI, "X");
+        Player player2 = new RobotPlayer(gameUI, "O");
+        options.put("2", Arrays.asList(player1, player2));
+    }
 
-        switch (option) {
-            case '2':
-                player1 = new HumanPlayer(gameUI, "X");
-                player2 = new RobotPlayer(gameUI, "O");
-                break;
-            case '3':
-                player1 = new RobotPlayer(gameUI, "X");
-                player2 = new RobotPlayer(gameUI, "O");
-                break;
-            case '4':
-                player1 = new HumanPlayer(gameUI, "X");
-                player2 = new AlienPlayer(gameUI, "O");
-                break;
-        }
+    private void twoRobots() {
+        Player player1 = new RobotPlayer(gameUI, "X");
+        Player player2 = new RobotPlayer(gameUI, "O");
+        options.put("3", Arrays.asList(player1, player2));
+    }
 
-        return Arrays.asList(player1, player2);
+    private void humanAndAlien() {
+        Player player1 = new HumanPlayer(gameUI, "X");
+        Player player2 = new AlienPlayer(gameUI, "O");
+        options.put("4", Arrays.asList(player1, player2));
     }
 
     private String setOption() {
@@ -51,7 +60,7 @@ public class Menu {
     private boolean isInvalidOption(String option) {
         ArrayList<String> validIndexes = new ArrayList();
 
-        for (int index = 1; index <= Options.values().length; index++) {
+        for (int index = 1; index <= options.size(); index++) {
             validIndexes.add(Integer.toString(index));
         }
 
